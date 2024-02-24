849e67b530e331cb500853d82e9f6a75145a930a
unknown <blairbowen35@gmail.com>
2024-02-24 12:58:01 -0500

    some updates, not really sure what changed

--- endpoints/basketball/ncaam.py
+++ endpoints/basketball/ncaam.py
@@ -19,8 +19,11 @@ team_column_headers = [
     "ABBREVIATION",
     "SHORT_NAME",
     "TEAM_NAME",
-    "COLOR",
-    "ALT_COLOR",
+    "WINS",
+    "LOSSES",
+    "STANDING",
+    "STREAK",
+    "GB",
 ]
 
 stats_column_headers = [
@@ -83,39 +86,37 @@ stats_column_headers = [
 ]
 
 
-def get_teams(refresh=False):
+def get_teams(refresh=False, year=2024):
     if refresh:
-        data = utils.fetch_data(config.mens_college_basketball_teams_url)
+        data = utils.fetch_data(config.mens_college_basketball_standings_url.replace("*", str(year)))
         df = pd.DataFrame(columns=team_column_headers)
-        conferences = data["sports"][0]["leagues"][0]["groups"]
+        conferences = data["children"]
         for conference in conferences:
             print(conference["name"])
-            for teams in conference["teams"]:
+            for team in conference["standings"]["entries"]:
                 row_data = {
-                    "ID": teams["id"],
-                    "GROUP_ID": conference["uid"].split("g:")[1],
-                    "DISPLAY_NAME": teams["displayName"],
-                    "SLUG": teams["slug"],
-                    "ABBREVIATION": teams["abbreviation"],
-                    "SHORT_NAME": teams["shortDisplayName"],
-                    "TEAM_NAME": teams["name"],
-                    "COLOR": (
-                        teams["color"] if "alternateColor" in teams.keys() else "000000"
-                    ),
-                    "ALT_COLOR": (
-                        teams["alternateColor"]
-                        if "alternateColor" in teams.keys()
-                        else "ffffff"
-                    ),
+                    "ID": team["team"]["id"],
+                    "GROUP_ID": conference["id"],
+                    "DISPLAY_NAME": team["team"]["displayName"],
+                    "SLUG": team["team"]["displayName"].replace(" ", "_").lower(),
+                    "ABBREVIATION": team["team"]["abbreviation"],
+                    "SHORT_NAME": team["team"]["shortDisplayName"],
+                    "TEAM_NAME": team["team"]["name"],
+                    "WINS": team["stats"][11]["value"],
+                    "LOSSES": team["stats"][4]["value"],
+                    "STANDING": team["stats"][5]["value"],
+                    "STREAK": team["stats"][9]["value"],
+                    "GB": team["stats"][2]["value"],
                 }
 
-                df = pd.concat([df, pd.DataFrame([row_data])], ignore_index=False)
+                df = pd.concat([df, pd.DataFrame([row_data])], ignore_index=True)
 
         df.set_index("ID", inplace=True)
         df.to_csv("data\\basketball\\ncaam\\mens_college_basketball_teams.csv")
     else:
         return pd.read_csv(
-            "data\\basketball\\ncaam\\mens_college_basketball_teams.csv", index_col="ID"
+            "data\\basketball\\ncaam\\mens_college_basketball_teams.csv",
+            index_col="ID",
         )
 
 
@@ -130,7 +131,7 @@ def get_conferences(refresh=False):
                 "SHORT_NAME": conference["shortName"].replace(" ", "-").lower(),
             }
 
-            df = pd.concat([df, pd.DataFrame([row_data])], ignore_index=False)
+            df = pd.concat([df, pd.DataFrame([row_data])], ignore_index=True)
 
         df.set_index("ID", inplace=True)
         df.to_csv("data\\basketball\\ncaam\\mens_college_basketball_conferences.csv")
@@ -149,138 +150,111 @@ def get_team_stats(refresh=False, year=2024):
         df = pd.DataFrame(columns=stats_column_headers)
         for team in teams_list["teams"]:
 
-            TO_PCT = team["categories"][4]["values"][4] / (
-                team["categories"][4]["values"][7]
-                + 0.44 * team["categories"][4]["values"][11]
-                + team["categories"][4]["values"][4]
-            )
-            FT_RATE = (
-                team["categories"][4]["values"][11] / team["categories"][4]["values"][7]
-            )
-            PACE = 0.5 * (
-                (
+            try:
+                TO_PCT = team["categories"][4]["values"][4] / (
                     team["categories"][4]["values"][7]
-                    + 0.4 * team["categories"][4]["values"][11]
-                    - 1.07
-                    * (
-                        team["categories"][4]["values"][12]
-                        / (
-                            team["categories"][4]["values"][12]
-                            + team["categories"][3]["values"][1]
-                        )
-                    )
-                    * (
-                        team["categories"][4]["values"][7]
-                        - team["categories"][4]["values"][6]
-                    )
+                    + 0.44 * team["categories"][4]["values"][11]
                     + team["categories"][4]["values"][4]
                 )
-                + 0.5
-                * (
-                    (
-                        team["categories"][5]["values"][7]
-                        + 0.4 * (team["categories"][5]["values"][11])
-                        - 1.07
-                        * (team["categories"][5]["values"][12])
-                        / (
-                            team["categories"][5]["values"][12]
-                            + team["categories"][2]["values"][1]
-                        )
-                    )
-                    * (
-                        team["categories"][5]["values"][7]
-                        - team["categories"][5]["values"][8]
-                    )
+                FT_RATE = (
+                    team["categories"][4]["values"][11] / team["categories"][4]["values"][7]
+                )
+                PACE = 0.96 * (
+                    team["categories"][4]["values"][7]
+                    + team["categories"][4]["values"][4]
+                    + 0.475 * team["categories"][4]["values"][11]
+                    - team["categories"][4]["values"][12]
+                )
+                OPP_TO_PCT = team["categories"][5]["values"][4] / (
+                    team["categories"][5]["values"][7]
+                    + 0.475 * team["categories"][5]["values"][11]
                     + team["categories"][5]["values"][4]
                 )
-            )
-            OPP_TO_PCT = team["categories"][5]["values"][4] / (
-                team["categories"][5]["values"][7]
-                + 0.44 * team["categories"][5]["values"][11]
-                + team["categories"][5]["values"][4]
-            )
-            OPP_FT_RATE = (
-                team["categories"][5]["values"][11] / team["categories"][5]["values"][7]
-            )
-            OFF_EFF = 100 * (team["categories"][4]["values"][0] / PACE)
-            DEF_EFF = 100 * (team["categories"][5]["values"][0] / PACE)
-            NET_RTG = OFF_EFF - DEF_EFF
-            EFF_FG_PCT = (
-                team["categories"][4]["values"][6]
-                + 0.5 * team["categories"][4]["values"][8]
-            ) / team["categories"][4]["values"][7]
-            OPP_EFF_FG_PCT = (
-                team["categories"][5]["values"][6]
-                + 0.5 * team["categories"][5]["values"][8]
-            ) / team["categories"][5]["values"][7]
-            FOUR_FAC = (
-                0.4 * EFF_FG_PCT
-                + 0.25 * TO_PCT
-                + 0.2 * team["categories"][4]["values"][23]
-                + 0.15 * FT_RATE
-            )
+                OPP_FT_RATE = (
+                    team["categories"][5]["values"][11] / team["categories"][5]["values"][7]
+                )
+                OFF_EFF = 100 * (team["categories"][4]["values"][0] / PACE)
+                DEF_EFF = 100 * (team["categories"][5]["values"][0] / PACE)
+                NET_RTG = OFF_EFF - DEF_EFF
+                EFF_FG_PCT = (
+                    team["categories"][4]["values"][6]
+                    + 0.5 * team["categories"][4]["values"][8]
+                ) / team["categories"][4]["values"][7]
+                OPP_EFF_FG_PCT = (
+                    team["categories"][5]["values"][6]
+                    + 0.5 * team["categories"][5]["values"][8]
+                ) / team["categories"][5]["values"][7]
+                FOUR_FAC = (
+                    0.4 * EFF_FG_PCT
+                    + 0.25 * TO_PCT
+                    + 0.2 * team["categories"][4]["values"][23]
+                    + 0.15 * FT_RATE
+                )
 
-            row_data = {
-                "ID": team["team"]["id"],
-                "GP": team["categories"][0]["values"][7],
-                "PTS": team["categories"][4]["values"][0],
-                "REB": team["categories"][0]["values"][1],
-                "AST": team["categories"][4]["values"][13],
-                "STL": team["categories"][2]["values"][2],
-                "BLK": team["categories"][2]["values"][3],
-                "TO": team["categories"][4]["values"][4],
-                "FG%": team["categories"][4]["values"][1],
-                "3P%": team["categories"][4]["values"][2],
-                "FT%": team["categories"][4]["values"][3],
-                "FGM": team["categories"][4]["values"][6],
-                "FGA": team["categories"][4]["values"][7],
-                "3PM": team["categories"][4]["values"][8],
-                "3PA": team["categories"][4]["values"][9],
-                "FTM": team["categories"][4]["values"][10],
-                "FTA": team["categories"][4]["values"][11],
-                "OR": team["categories"][4]["values"][12],
-                "DR": team["categories"][2]["values"][1],
-                "OR%": team["categories"][4]["values"][23],
-                "TO%": TO_PCT,
-                "AST/TO": team["categories"][0]["values"][6],
-                "PF": team["categories"][0]["values"][8],
-                "SC_EFF": team["categories"][4]["values"][24],
-                "SH_EFF": team["categories"][4]["values"][25],
-                "FT_RATE": FT_RATE,
-                "PACE": PACE,
-                "OPP_PTS": team["categories"][5]["values"][0],
-                "OPP_REB": team["categories"][1]["values"][1],
-                "OPP_AST": team["categories"][5]["values"][13],
-                "OPP_STL": team["categories"][3]["values"][2],
-                "OPP_BLK": team["categories"][3]["values"][3],
-                "OPP_TO": team["categories"][5]["values"][4],
-                "OPP_FG%": team["categories"][5]["values"][1],
-                "OPP_3P%": team["categories"][5]["values"][2],
-                "OPP_FT%": team["categories"][5]["values"][3],
-                "OPP_FGM": team["categories"][5]["values"][6],
-                "OPP_FGA": team["categories"][5]["values"][7],
-                "OPP_3PM": team["categories"][5]["values"][8],
-                "OPP_3PA": team["categories"][5]["values"][9],
-                "OPP_FTM": team["categories"][5]["values"][10],
-                "OPP_FTA": team["categories"][5]["values"][11],
-                "OPP_OR": team["categories"][5]["values"][12],
-                "OPP_DR": team["categories"][3]["values"][1],
-                "OPP_OR%": team["categories"][5]["values"][23],
-                "OPP_TO%": OPP_TO_PCT,
-                "OPP_AST/TO": team["categories"][1]["values"][6],
-                "OPP_PF": team["categories"][1]["values"][8],
-                "OPP_SC_EFF": team["categories"][5]["values"][24],
-                "OPP_SH_EFF": team["categories"][5]["values"][25],
-                "OPP_FT_RATE": OPP_FT_RATE,
-                "OFF_EFF": OFF_EFF,
-                "DEF_EFF": DEF_EFF,
-                "NET_RTG": NET_RTG,
-                "EFF_FG%": EFF_FG_PCT,
-                "OPP_EFF_FG%": OPP_EFF_FG_PCT,
-                "4FAC": FOUR_FAC,
-            }
+                row_data = {
+                    "ID": team["team"]["id"],
+                    "GP": team["categories"][0]["values"][7],
+                    "PTS": team["categories"][4]["values"][0],
+                    "REB": team["categories"][0]["values"][1],
+                    "AST": team["categories"][4]["values"][13],
+                    "STL": team["categories"][2]["values"][2],
+                    "BLK": team["categories"][2]["values"][3],
+                    "TO": team["categories"][4]["values"][4],
+                    "FG%": team["categories"][4]["values"][1],
+                    "3P%": team["categories"][4]["values"][2],
+                    "FT%": team["categories"][4]["values"][3],
+                    "FGM": team["categories"][4]["values"][6],
+                    "FGA": team["categories"][4]["values"][7],
+                    "3PM": team["categories"][4]["values"][8],
+                    "3PA": team["categories"][4]["values"][9],
+                    "FTM": team["categories"][4]["values"][10],
+                    "FTA": team["categories"][4]["values"][11],
+                    "OR": team["categories"][4]["values"][12],
+                    "DR": team["categories"][2]["values"][1],
+                    "OR%": team["categories"][4]["values"][23],
+                    "TO%": TO_PCT,
+                    "AST/TO": team["categories"][0]["values"][6],
+                    "PF": team["categories"][0]["values"][8],
+                    "SC_EFF": team["categories"][4]["values"][24],
+                    "SH_EFF": team["categories"][4]["values"][25],
+                    "FT_RATE": FT_RATE,
+                    "PACE": PACE,
+                    "OPP_PTS": team["categories"][5]["values"][0],
+                    "OPP_REB": team["categories"][1]["values"][1],
+                    "OPP_AST": team["categories"][5]["values"][13],
+                    "OPP_STL": team["categories"][3]["values"][2],
+                    "OPP_BLK": team["categories"][3]["values"][3],
+                    "OPP_TO": team["categories"][5]["values"][4],
+                    "OPP_FG%": team["categories"][5]["values"][1],
+                    "OPP_3P%": team["categories"][5]["values"][2],
+                    "OPP_FT%": team["categories"][5]["values"][3],
+                    "OPP_FGM": team["categories"][5]["values"][6],
+                    "OPP_FGA": team["categories"][5]["values"][7],
+                    "OPP_3PM": team["categories"][5]["values"][8],
+                    "OPP_3PA": team["categories"][5]["values"][9],
+                    "OPP_FTM": team["categories"][5]["values"][10],
+                    "OPP_FTA": team["categories"][5]["values"][11],
+                    "OPP_OR": team["categories"][5]["values"][12],
+                    "OPP_DR": team["categories"][3]["values"][1],
+                    "OPP_OR%": team["categories"][5]["values"][23],
+                    "OPP_TO%": OPP_TO_PCT,
+                    "OPP_AST/TO": team["categories"][1]["values"][6],
+                    "OPP_PF": team["categories"][1]["values"][8],
+                    "OPP_SC_EFF": team["categories"][5]["values"][24],
+                    "OPP_SH_EFF": team["categories"][5]["values"][25],
+                    "OPP_FT_RATE": OPP_FT_RATE,
+                    "OFF_EFF": OFF_EFF,
+                    "DEF_EFF": DEF_EFF,
+                    "NET_RTG": NET_RTG,
+                    "EFF_FG%": EFF_FG_PCT,
+                    "OPP_EFF_FG%": OPP_EFF_FG_PCT,
+                    "4FAC": FOUR_FAC,
+                }
 
-            df = pd.concat([df, pd.DataFrame([row_data])], ignore_index=False)
+                df = pd.concat([df, pd.DataFrame([row_data])], ignore_index=True)
+            except Exception as e:
+                team_name = team["team"]["displayName"]
+                print(f"Issue for {team_name}\n{e}")
 
         df.set_index("ID", inplace=True)
         df = df.loc[df["GP"] > 5]
--- main.py
+++ main.py
@@ -9,12 +9,19 @@ import utilities.api as utils
 import utilities.basketball.ncaam.api as ncaam_api
 
 df = pd.DataFrame(columns=ncaam_api.stats_column_headers)
+years = [2015, 2016, 2017, 2018, 2019, 2021, 2022, 2023, 2024]
 
 if __name__ == "__main__":
     # start = time.time()
     # ncaam_api.iterate_conferences(ncaam.get_team_stats, conference_selector=["sec"])
     # end = time.time()
     # print(end - start)
-    print(ncaam.get_team_stats().shape)
+    print(ncaam.get_teams())
+    print(ncaam.get_team_stats(True))
+    print(ncaam.get_conferences())
+    # for year in years:
+    #     print(ncaam.get_team_stats(True, year))
+    #     print(ncaam.get_teams(True, year))
+    # print(ncaam.get_conferences(True))
 
     pass
